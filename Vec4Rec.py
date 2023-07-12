c5e53dcee843dfb843f3ea1e9995bf1c6141c212
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-07-11 22:46:45 -0400

    add dataclasses

--- Vec4Rec.py
+++ Vec4Rec.py
@@ -1,4 +1,15 @@
 from random import randint, random
+from dataclasses import dataclass
+
+@dataclass
+class Space:
+    dim: int
+    size: int
+
+@dataclass
+class Point:
+    id: int
+    cords: list[float]
 
 class Vec4Rec:
     def __init__(self, dim, size_of_space):
@@ -6,9 +17,13 @@ class Vec4Rec:
         self.points = []
         self.size_of_space = size_of_space
 
-    def place_point(self, cords = None):
+    def place_point(self, id: int = None, cords: list[float] = None):
         if cords:
             if len(cords) == self.dim:
                 raise ValueError("Invalid number of dimensions")
         else:
             cords = [randint(-self.size_of_space, self.size_of_space - 1) + random() for _ in range(self.dim)]
+
+
+v = Vec4Rec(4, 100)
+v.place_point()
