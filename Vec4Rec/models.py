eb716975587bba7cdc8924ccd6387849adb4aad5
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-07-12 00:24:59 -0400

    equality

---
+++ Vec4Rec/models.py
@@ -0,0 +1,26 @@
+from random import randint, random
+from dataclasses import dataclass
+
+@dataclass(eq=False, order=False, match_args=False)
+class Space:
+    dim: int
+    size: int
+
+@dataclass(eq=False, order=False, match_args=False)
+class Point:
+    space: Space
+    id: int
+    cords: list[float] = None
+
+    def __post_init__(self) -> None:
+        if self.cords:
+            if len(self.cords) == self.space.dim:
+                raise ValueError("Invalid number of dimensions")
+        else:
+            self.cords = [randint(-self.space.size, self.space.size - 1) + random() for _ in range(self.space.dim)]
+
+    def __str__(self) -> str:
+        return f"{self.id}"
+
+    def __eq__(self, __value: object) -> bool:
+        return self.cords == __value.cords
