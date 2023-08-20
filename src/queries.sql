a0fe59435d6bf38eeaae68044c892677de0f47fc
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2023-08-20 12:51:16 -0400

    table name

--- src/queries.sql
+++ src/queries.sql
@@ -1,14 +1,14 @@
 -- make_vector_table
-CREATE TABLE IF NOT EXISTS vector_table (
+CREATE TABLE IF NOT EXISTS VECTOR_TABLE (
     id SERIAL PRIMARY KEY,
     vector CLOB NOT NULL
 );
 -- get_point_by_id
 SELECT vector
-FROM vector_table
+FROM VECTOR_TABLE
 WHERE id == {0};
 -- delete_point_by_id
-DELETE FROM vector_table
+DELETE FROM VECTOR_TABLE
 WHERE id == {0};
 -- add_point
-INSERT INTO vector_table
+INSERT INTO VECTOR_TABLE
